5b0e60b01c79fdfd82e09a707b8f09fca41a4099
Raffaele Prince <raffaele.princee@gmail.com>
2025-07-26 11:44:23 +0200

    Fixed relative path for parent in pom modules

--- business-modules/order-service/order-core/src/main/java/raff/stein/orderservice/service/OrderService.java
+++ business-modules/order-service/order-core/src/main/java/raff/stein/orderservice/service/OrderService.java
@@ -1,16 +1,7 @@
 package raff.stein.orderservice.service;
 
-import raff.stein.proposalservice.dataasync.model.ProposalUpdatedEvent;
 
 public class OrderService {
     
-    public void createOrder() {
-        // Logic to create an order
-        // read from ProposalTopic
-        ProposalUpdatedEvent proposalUpdatedEvent = readProposalUpdatedEvent();
-    }
 
-    private ProposalUpdatedEvent readProposalUpdatedEvent() {
-        return new ProposalUpdatedEvent().proposalId(1L);
-    }
 }
