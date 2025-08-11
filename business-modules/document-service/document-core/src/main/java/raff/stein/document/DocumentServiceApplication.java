3d119933fe91b05e6cb8fd7382217118e7a43627
Raffaele Prince <raffaele.princee@gmail.com>
2025-08-11 16:01:50 +0200

    Added document-service modules and base app

--- business-modules/document-service/document-core/src/main/java/raff/stein/document/DocumentServiceApplication.java
+++ business-modules/document-service/document-core/src/main/java/raff/stein/document/DocumentServiceApplication.java
@@ -1,4 +1,4 @@
-package raff.stein.customer;
+package raff.stein.document;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +7,11 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {
         "raff.stein.platformcore", // Include platform core for shared components like security
-        "raff.stein.customer"
+        "raff.stein.document"
 })
-public class CustomerServiceApplication {
+public class DocumentServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CustomerServiceApplication.class, args);
+        SpringApplication.run(DocumentServiceApplication.class, args);
     }
 }
 
